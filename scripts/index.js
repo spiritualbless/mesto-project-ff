cf133e7dae255336fad5c22c5f83916360c06991
Jaroslaw Svakhin <yaroslav.svakhin@yandex.ru>
2024-12-12 19:53:23 +0300

    reload

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
 // @todo: Темплейт карточки
 
+function createCard(card){
+    const template = document.querySelector('#card-template').content;
+    const card = template.cloneNode(true);
+
+    const cardImage = card.querySelector('.card__image');
+    const cardTitle = card.querySelector('.card__title');
+    const deleteButton = card.querySelector('.card__delete-button');
+
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
+    cardTitle.textContent = card.name;
+
+    deleteButton.addEventListener('click', deleteCard(card));
+
+    return card;
+}
+
 // @todo: DOM узлы
+
+
 
 // @todo: Функция создания карточки
 
+const cardsContainer = document.querySelector('.places__list');
+const cardsArr = initialCards;
+
+
+
 // @todo: Функция удаления карточки
 
+function deleteCard(card){
+    card.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
